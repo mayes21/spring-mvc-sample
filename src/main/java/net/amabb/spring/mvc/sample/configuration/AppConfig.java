f1e1329249dc0fe770663a77cd30a6e5b9d7b16a
Administrator <admin@example.com>
2016-02-01 17:45:03 +0100

    hibernate + spring configuration classes / properties

---
+++ src/main/java/net/amabb/spring/mvc/sample/configuration/AppConfig.java
@@ -0,0 +1,37 @@
+package net.amabb.spring.mvc.sample.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by amabb on 01/02/16.
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "net.amabb.spring.mvc.sample.springmvc")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+}
